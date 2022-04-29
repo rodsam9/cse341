e61dcd0dba19eaefb4254ffcba2f20a189fc2d39
rodsam9 <sr_odriguez9@yahoo.com>
2022-04-29 13:24:41 -0600

    added contacts to routes

---
+++ routes/contacts.js
@@ -0,0 +1,21 @@
+const routes = require('express').Router();
+
+routes.get('/', (req, res) => {
+
+    const dotenv = require('dotenv');
+    dotenv.config();
+
+    const MongoClient = require('mongodb').MongoClient;
+
+    MongoClient.connect(process.env.MONGODB_URI, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("contacts");
+        dbo.collection("contacts").find().toArray(function (err, result) {
+            if (err) throw err;
+            res.json(result);
+            db.close();
+        });
+    });
+});
+
+module.exports = routes;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,5 @@
 const routes = require('express').Router();
 
-routes.get('/', (req, res) => {
-    res.send('Josh Rod');
-});
+routes.use('/contacts', require('./contacts'));
 
 module.exports = routes;
