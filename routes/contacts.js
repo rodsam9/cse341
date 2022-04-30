6e0c189a012897b0094de4e4dcce21575aa1701e
rodsam9 <sr_odriguez9@yahoo.com>
2022-04-30 00:29:32 -0600

    GET request for all contacts and single contact info

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,12 +1,26 @@
 const routes = require('express').Router();
 const connect = require('../db/connect');
-
+const ObjectId = require('mongodb').ObjectId;
 routes.get('/', (req, res) => {
-    connect.getCollection().find().toArray((err, result) => {
-        if (err) throw err;
-        res.json(result);
-        console.log("Contact Info Successful");
+
+    const results = connect.getCollection().find();
+
+    results.toArray().then((documents) => {
+        res.status(200).json(documents);
+        console.log(`Returned All Contacts`);
     });
+});
+
+routes.get('/:id', (req, res) => {
+    const contactId = new ObjectId(req.params.id);
+
+    const results = connect.getCollection().find({ _id: contactId });
+
+    results.toArray().then((documents) => {
+        res.status(200).json(documents[0]);
+        console.log(`Returned Contact ${req.params.id}`);
+    });
+
 });
 
 
