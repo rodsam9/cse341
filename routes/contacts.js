d7e60ba4393f8205f72e956da734247bd2c58dbb
rodsam9 <sr_odriguez9@yahoo.com>
2022-05-06 20:47:46 -0600

    installed lint and prettier

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,6 +1,7 @@
 const routes = require('express').Router();
 const connect = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
+
 routes.get('/', (req, res) => {
 
     const results = connect.getCollection().find();
