97681c7b8c17e514b4f6f567a4f36c3b3cd563f8
rodsam9 <sr_odriguez9@yahoo.com>
2022-05-13 00:35:22 -0600

    fixed url heroku

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
         title: 'My API',
         description: 'Temple API',
     },
-    host: 'https://cse341-contact-info.herokuapp.com',
+    host: 'cse341-contact-info.herokuapp.com',
     schemes: ['http'],
 };
 
